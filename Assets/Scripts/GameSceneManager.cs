510bd889e672fb02932f61280362ee7ba9037d6f
Ekhalios <leandre.sentenac@epitech.eu>
2023-11-28 11:01:38 +0100

    add menu escape in game

--- Assets/Scripts/GameSceneManager.cs
+++ Assets/Scripts/GameSceneManager.cs
@@ -5,13 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class GameSceneManager : MonoBehaviour
 {
-
+    public GameObject panelMenu;
+    public PlayerMovement movement;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(0);
+            if (panelMenu.activeSelf)
+            {
+                panelMenu.SetActive(false);
+            } else
+            {
+                panelMenu.SetActive(true);
+            }
+            movement.switchControlable();
         }
+    }
+
+    public void backMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void backGame()
+    {
+        panelMenu.SetActive(false);
+        movement.switchControlable();
     }
 }
